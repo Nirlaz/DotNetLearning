04c12a3b59fa7d4fb2f4f13f24d046ec84e5bd74
Nirlaz Gyawali <53090074+Nirlaz@users.noreply.github.com>
2025-02-28 15:07:30 +0545

    delte

--- StudentsMangement/Models/UpdateStudent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentsMangement.Models
-{
-    public class UpdateStudent
-    {
-        public string? Name { get; set; }
-        public int Grade { get; set; }
-        public string? Gender { get; set; }
-    }
-}
